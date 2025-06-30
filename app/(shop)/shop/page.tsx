386253c84d83bea8997bf1071d023e653e88f5db
Miss-nonso <chinonsodaniels@yahoo.com>
2025-06-30 17:42:17 +0100

    feat: footer added

--- app/(shop)/shop/page.tsx
+++ app/(shop)/shop/page.tsx
@@ -45,7 +45,7 @@ const Shop = () => {
       <header className="shop-header h-[250px] w-full relative grid place-items-center">
         <div className="overlay bg-[#00000066] h-full w-full absolute"></div>
         <div className="text-white z-10 text-center grid gap-4">
-          <h1 className="text-6xl font-extrabold">
+          <h1 className="text-6xl font-extrabold mt-8 sm:mt-0">
             MAR<span className="italic text-5xl text-yellow-100">K</span>ERT
             AFRICA
           </h1>
@@ -54,16 +54,29 @@ const Shop = () => {
       </header>
       <Link
         href="/cart"
-        className="absolute right-8 top-4 bg-white px-6 py-4 font-bold text-xl rounded-3xl"
+        className="absolute border border-black right-4 sm:right-8 top-2 sm:top-4 bg-white px-5 py-1.5 sm:px-6 sm:py-4 font-bold text-lg sm:text-xl rounded-3xl"
       >
         Go to Cart
       </Link>
-      <div className="flex place-items-center justify-center gap-6 flex-wrap mx-auto mt-6 py-6 px-4">
+      <div className="flex place-items-center justify-center gap-6 flex-wrap mb-20 mx-auto mt-6 py-6 px-4">
         {" "}
         {products.map((product, index) => (
           <ProductCard key={index} product={product} />
         ))}
       </div>
+
+      <footer className="bg-black h-[200px] w-full relative grid place-items-center">
+        <div className="overlay bg-[#00000066] h-full w-full absolute"></div>
+        <div className="text-white z-10 text-center grid gap-4">
+          <h1 className="text-3xl font-extrabold mt-8 sm:mt-0">
+            SHOP<span className="italic  text-yellow-100"> NOW</span>. SHOP SAFE
+          </h1>
+          <p className="text-base">
+            The Shopping Experience you can Trust,{" "}
+            <span className="italic  text-yellow-100"> always</span>{" "}
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -45,7 +45,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
   if (!isClient) return null;
 
   return (
-    <div className="product-card hover:scale-[1.01] w-full sm:w-[250px] max-w-[300px] self-start aspect-square rounded-3xl relative transition-all ease-in-out duration-300">
+    <div className="product-card hover:scale-[1.01] w-full sm:w-[250px] sm:max-w-[300px] self-start aspect-square rounded-3xl relative transition-all ease-in-out duration-300">
       <Image
         src={product.img}
         alt={product.name}
@@ -70,7 +70,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
             onClick={handleCartToggle}
             className={` ${
               isInCart && "text-sm hover:text-[14.1px]"
-            } bg-white text-sm text-black font-bold rounded-2xl cursor-pointer hover:text-[14.9px] hover:rounded-lg py-2 px-3 transition-all duration-300 ease-in-out hover:border-1 hover:border-black  `}
+            } bg-yellow-300 border border-black text-sm text-black font-bold rounded-sm shadow-md cursor-pointer hover:text-[14.1px] hover:rounded-lg py-2 px-3 transition-all duration-300 ease-in-out hover:border-2 hover:border-black`}
           >
             {isInCart ? "Remove from cart" : "Add to cart"}
           </button>
